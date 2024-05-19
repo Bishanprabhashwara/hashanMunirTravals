aea5ff50ed8adb90388ea3caf37fca35764ea5c8
Bishan prabhashwara <bishanprabhashwara@gmail.com>
2024-05-19 20:44:16 +0530

    commit2

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './Services.css'
+import YasakaShrine from '../../assets/images/YasakaShrine.JPG'
 
 function Services() {
   return (
-    <div className='services'>Services Services Services Services Services Services Services Services Services Services Services Services Services Services Services Services Services Services Services Services </div>
+    <div className='services'>
+      <div>
+      
+        <div>
+          <img src={YasakaShrine} className='YasakaShrineimg'/>
+          <div className='YasakaShrineinfo'>
+          <h1><a href='#' className='earlybirda'>Early Bird Tour </a></h1>
+          <br/>
+            <ul>
+              <li>Visit two of the most famous places in Japan, the Fushimi Inari Shrine and the Kiyomizu Temple</li>
+              <li>Beat the crowds with a 6 o'clock start </li>
+              <li>Choose from a private tour or a small group tour with a maximum of eight participants</li>
+              <li>From 9,500 yen / person ($65 USD)</li>
+            </ul>
+            <div className='YasakaShrinebutton'><button className='btn btn-outline-success YasakaShrinebutton'>Book Now</button></div>
+          </div>
+          
+        </div>
+        
+        
+      </div>
+      <hr/>
+        <div>
+          <h1>sad</h1>
+        </div>
+    </div>
   )
 }
 
